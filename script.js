9f259af2638638f3920005724efd698997a3f8af
JoyadityaNath <joyadityan@gmail.com>
2025-10-30 18:31:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function performSearch() {
 
     results.scrollIntoView({ behavior: "smooth" });
 }
+function performSearch() {
+  let query = document.getElementById("searchInput").value.toLowerCase();
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let tags = card.getAttribute("data-tags");
+    card.style.display = tags.includes(query) ? "block" : "none";
+  });
+}
